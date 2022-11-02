013beeb32edbb7142f7893c9689543563b965a74
d3vdor <devdor.09@gmail.com>
2022-11-02 16:27:36 -0600

    Arranque de proyecto con estructura basica

---
+++ main.py
@@ -0,0 +1,30 @@
+def menu():
+    pass
+
+
+def suma():
+    pass
+
+
+def resta():
+    pass
+
+
+def producto():
+    pass
+
+
+def division():
+    pass
+
+
+def residuo():
+    pass
+
+
+def main():
+    print("Programa finalizado")
+
+
+if __name__ == '__main__':
+    main()
