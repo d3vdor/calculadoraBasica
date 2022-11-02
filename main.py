7716daa9137302063d0055e50dca0bb290f8a3ac
d3vdor <devdor.09@gmail.com>
2022-11-02 16:37:16 -0600

    Agregando logica a menu y agregando librerias

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+# Librerias
+import os
+import time
+
+
 def menu():
-    pass
+    while True:
+        print("""
+            --- Menu ---
+            1. Suma
+            2. Resta
+            3. Multiplicacion
+            4. Division
+            5. Residuo
+            6. Salir
+        """)
+        opc = int(input("Seleccione una opcion: "))
+        n1 = int(input("Digite un valor para el numero 1: "))
+        n2 = int(input("Digite un valor para el numero 2: "))
+        if opc == 1:
+            suma(n1,n2)
+        elif opc == 2:
+            resta(n1,n2)
+        elif opc == 3:
+            producto(n1,n2)
+        elif opc == 4:
+            division(n1,n2)
+        elif opc == 5:
+            residuo(n1,n2)
+        elif opc == 6:
+            break
+        else:
+            print("Esa opcion no es correcta")
+
+
 
 
 def suma():
@@ -23,6 +56,7 @@ def residuo():
 
 
 def main():
+    menu()
     print("Programa finalizado")
 
 
